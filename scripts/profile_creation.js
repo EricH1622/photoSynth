a2863841fecb8457e75010de73e939a2a24534a2
james01123 <erichemming11@gmail.com>
2022-04-03 21:06:19 -0700

    overhaul most pages to fit theme.  Made Profile page fill in with info from firestore database.

--- scripts/profile_creation.js
+++ scripts/profile_creation.js
@@ -64,3 +64,32 @@ function editUserInfo() {
 
     document.getElementById('personalInfoFields').disabled = true;
 }
+
+function insertName(){
+    // to check if the user is logged in:
+     firebase.auth().onAuthStateChanged(user =>{
+         if (user){
+             console.log(user.uid); // let me to know who is the user that logged in to get the UID
+            currentUser = db.collection("users").doc(user.uid); // will to to the firestore and go to the document of the user
+            currentUser.get().then(userDoc=>{
+                //get the user name
+                var user_Name= userDoc.data().name;
+                var user_Bio = userDoc.data().bio;
+                var user_City = userDoc.data().city;
+                var user_Gear = userDoc.data().gear;
+                console.log(user_Name);
+                $("#name-goes-here").text(user_Name); //jquery
+                $("#bio-goes-here").text(user_Bio);
+                $("#city-goes-here").text(user_City);
+                $("#gear-goes-here").text(user_Gear);
+
+
+                // document.getElementByID("name-goes-here").innetText=user_Name;
+            })    
+        }
+    
+     })
+    }
+    insertName();
+
+
